33b17147ca39f018de051c4ba883409717ddab0f
Hanafee <hanafee414@gmail.com>
2021-03-21 11:26:20 +0700

    ~ add user, config and database module into app module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from "@nestjs/config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = app.get(ConfigService)
+
+  await app.listen(config.get<number>("app.port"));
+  console.log(`=>> \n\n service start on ${await app.getUrl()} \n\n=>>`);
 }
 bootstrap();
